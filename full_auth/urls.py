1e8537a208887f577e6bfb66518bedaf5110a634
Guillaume Nadal <guillaume.nadal13@gmail.com>
2025-02-07 11:21:12 -0500

    update gitignore and modify url

--- full_auth/urls.py
+++ full_auth/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('users.urls')),
     path('api/dashboard/', include('dashboard.urls')),
-    path("api/samples/", include("samples.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/samples/', include('samples.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
